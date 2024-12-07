0bb6c3b6646a178cdc910ed322cdb0bfd628833f
Omar Cusma Fait <31404553+Omega97@users.noreply.github.com>
2024-12-07 12:14:10 +0100

    occlusion and void perturbaiton

--- xai_rai_units/scripts/test_perturbations.py
+++ xai_rai_units/scripts/test_perturbations.py
@@ -1,20 +1,59 @@
 import matplotlib.pyplot as plt
-from xai_rai_units.src.perturbations import blur_image_linspace
+from xai_rai_units.src import perturbations as pert
 from xai_rai_units.src.paths import IMAGE_DIR
 from xai_rai_units.src.alexnet import load_images
 
 
-def test_blur_image_linspace(image_dir=IMAGE_DIR, magnitude=10, n=5):
-    """Test the function blur_image_linspace."""
+def test_noisy_image_linspace(image_dir=IMAGE_DIR, magnitude=.5, n=5):
+    """Test the function noisy_image_linspace."""
     image = load_images(str(image_dir), n=1)[0]
-    blurred_images = blur_image_linspace(image, magnitude=magnitude, n=n)
+    perturbed_images = pert.noisy_image_linspace(image, magnitude=magnitude, n=n)
     fig, ax = plt.subplots(nrows=1, ncols=n, figsize=(20, 2))
     for i in range(n):
-        ax[i].set_title(f'sigma={i/n*magnitude:.2f}')
-        ax[i].imshow(blurred_images[i].permute(1, 2, 0).numpy())
+        ax[i].set_title(f'{i+1}/{n}')
+        ax[i].imshow(perturbed_images[i].permute(1, 2, 0).numpy())
+        ax[i].axis('off')
+    plt.show()
+
+
+def test_blur_image_linspace(image_dir=IMAGE_DIR, magnitude=8, n=5):
+    """Test the function blur_image_linspace."""
+    image = load_images(str(image_dir), n=1)[0]
+    perturbed_images = pert.blur_image_linspace(image, magnitude=magnitude, n=n)
+    fig, ax = plt.subplots(nrows=1, ncols=n, figsize=(20, 2))
+    for i in range(n):
+        ax[i].set_title(f'{i}/{n}')
+        ax[i].imshow(perturbed_images[i].permute(1, 2, 0).numpy())
+        ax[i].axis('off')
+    plt.show()
+
+
+def test_occlusion_linspace(image_dir=IMAGE_DIR, magnitude=150, n=5):
+    """Test the function blur_image_linspace."""
+    image = load_images(str(image_dir), n=1)[0]
+    perturbed_images = pert.occlusion_linspace(image, magnitude=magnitude, n=n)
+    fig, ax = plt.subplots(nrows=1, ncols=n, figsize=(20, 2))
+    for i in range(n):
+        ax[i].set_title(f'{i+1}/{n}')
+        ax[i].imshow(perturbed_images[i].permute(1, 2, 0).numpy())
+        ax[i].axis('off')
+    plt.show()
+
+
+def test_the_void_linspace(image_dir=IMAGE_DIR, magnitude=10., n=5):
+    """Test the function the_void_linspace."""
+    image = load_images(str(image_dir), n=1)[0]
+    perturbed_images = pert.the_void_linspace(image, magnitude=magnitude, n=n)
+    fig, ax = plt.subplots(nrows=1, ncols=n, figsize=(20, 2))
+    for i in range(n):
+        ax[i].set_title(f'{i+1}/{n}')
+        ax[i].imshow(perturbed_images[i].permute(1, 2, 0).numpy())
         ax[i].axis('off')
     plt.show()
 
 
 if __name__ == '__main__':
-    test_blur_image_linspace()
+    # test_noisy_image_linspace()
+    # test_blur_image_linspace()
+    # test_occlusion_linspace()
+    test_the_void_linspace()
--- xai_rai_units/src/perturbations.py
+++ xai_rai_units/src/perturbations.py
@@ -52,3 +52,72 @@ def blur_image_linspace(image, magnitude, n, epsilon=1e-3):
         out.append(gaussian_blur(image))
 
     return torch.stack(out)
+
+
+def occlusion_linspace(image, magnitude, n, fill_value=0):
+    """
+    Creates a linspace of increasingly occluded images
+    :param image: a tensor of shape (n, C, H, W)
+    :param magnitude: maximum size of the occlusion (number of pixels)
+    :param n: number of samples to take
+    :param fill_value: value to fill the occlusion with
+    :return: a tensor of shape (n, C, H, W)
+    """
+
+    # choose a random occlusion region
+    _, H, W = image.shape
+    x1 = torch.linspace(W, W - magnitude, n, device=image.device).int() // 2
+    y1 = torch.linspace(H, H - magnitude, n, device=image.device).int() // 2
+    x2 = torch.linspace(W, W + magnitude, n, device=image.device).int() // 2
+    y2 = torch.linspace(H, H + magnitude, n, device=image.device).int() // 2
+    out = []
+    for i in range(n):
+        occluded = image.clone()
+        occluded[:, y1[i]:y2[i], x1[i]:x2[i]] = fill_value
+        out.append(occluded)
+
+    return torch.stack(out)
+
+
+def gaussian_kernel(kernel_size, sigma):
+    """
+    Creates a 2D Gaussian kernel.
+
+    Args:
+    kernel_size: The size of the kernel.
+    sigma: The standard deviation of the Gaussian distribution.
+
+    Returns:
+    A 2D Gaussian kernel.
+    """
+    x = torch.arange(-(kernel_size - 1) / 2, (kernel_size - 1) / 2 + 1)
+    y = x.unsqueeze(1)
+
+    kernel = torch.exp(-(x ** 2 + y ** 2) / (2 * sigma ** 2))
+    kernel /= kernel.sum()
+
+    return kernel
+
+
+def the_void_linspace(image, magnitude, n, fill_value=0):
+    """
+    Creates a linspace of increasingly voided images
+    :param image: a tensor of shape (n, C, H, W)
+    :param magnitude: maximum size of the void (number of pixels)
+    :param n: number of samples to take
+    :param fill_value: value to fill the void with
+    :return: a tensor of shape (n, C, H, W)
+    """
+    _, H, W = image.shape
+    x = torch.linspace(-1, 1, W, device=image.device)
+    y = torch.linspace(-1, 1, H, device=image.device)
+    y = y.unsqueeze(1)
+    print(x)
+
+    out = []
+    for i in range(n):
+        k = magnitude * (i / n)**2
+        mask = torch.exp(-(x ** 2 + y ** 2) * k)
+        out.append(image * mask + fill_value * (1 - mask))
+
+    return torch.stack(out)
