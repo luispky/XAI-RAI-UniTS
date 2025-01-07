b8e5a0c8b1eb3f59871d9c2fb479c4653671bb45
Omar Cusma Fait <31404553+Omega97@users.noreply.github.com>
2025-01-07 15:28:18 +0100

    renaming perturbations

--- xai_rai_units/scripts/test_perturbations.py
+++ xai_rai_units/scripts/test_perturbations.py
@@ -7,7 +7,7 @@ from xai_rai_units.src.alexnet import load_images
 def test_noisy_image_linspace(image_dir=IMAGE_DIR, magnitude=.5, n=5):
     """Test the function noisy_image_linspace."""
     image = load_images(str(image_dir), n=1)[0]
-    perturbed_images = pert.noisy_image_linspace(image, magnitude=magnitude, n=n)
+    perturbed_images = pert.gaussian_perturbation_linspace(image, magnitude=magnitude, n=n)
     fig, ax = plt.subplots(nrows=1, ncols=n, figsize=(20, 2))
     for i in range(n):
         ax[i].set_title(f'{i+1}/{n}')
@@ -19,7 +19,7 @@ def test_noisy_image_linspace(image_dir=IMAGE_DIR, magnitude=.5, n=5):
 def test_blur_image_linspace(image_dir=IMAGE_DIR, magnitude=8, n=5):
     """Test the function blur_image_linspace."""
     image = load_images(str(image_dir), n=1)[0]
-    perturbed_images = pert.blur_image_linspace(image, magnitude=magnitude, n=n)
+    perturbed_images = pert.blur_perturbation_linspace(image, magnitude=magnitude, n=n)
     fig, ax = plt.subplots(nrows=1, ncols=n, figsize=(20, 2))
     for i in range(n):
         ax[i].set_title(f'{i}/{n}')
@@ -31,7 +31,7 @@ def test_blur_image_linspace(image_dir=IMAGE_DIR, magnitude=8, n=5):
 def test_occlusion_linspace(image_dir=IMAGE_DIR, magnitude=150, n=5):
     """Test the function blur_image_linspace."""
     image = load_images(str(image_dir), n=1)[0]
-    perturbed_images = pert.occlusion_linspace(image, magnitude=magnitude, n=n)
+    perturbed_images = pert.occlusion_perturbation_linspace(image, magnitude=magnitude, n=n)
     fig, ax = plt.subplots(nrows=1, ncols=n, figsize=(20, 2))
     for i in range(n):
         ax[i].set_title(f'{i+1}/{n}')
@@ -43,7 +43,7 @@ def test_occlusion_linspace(image_dir=IMAGE_DIR, magnitude=150, n=5):
 def test_the_void_linspace(image_dir=IMAGE_DIR, magnitude=10., n=5):
     """Test the function the_void_linspace."""
     image = load_images(str(image_dir), n=1)[0]
-    perturbed_images = pert.the_void_linspace(image, magnitude=magnitude, n=n)
+    perturbed_images = pert.void_perturbation_linspace(image, magnitude=magnitude, n=n)
     fig, ax = plt.subplots(nrows=1, ncols=n, figsize=(20, 2))
     for i in range(n):
         ax[i].set_title(f'{i+1}/{n}')
